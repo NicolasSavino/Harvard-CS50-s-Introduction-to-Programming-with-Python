c2a4646b56f76f16d5a6652156cefdebd1c2ad54
Nicolas Savino <nicolassavino3@gmail.com>
2025-06-23 16:03:37 -0400

    Update lesson1.py

---
+++ lesson1.py
@@ -0,0 +1,40 @@
+# Ask user for their nanme
+name = input("what's your name")  # that's rizz bruh
+
+# Say hello to the user, Hi Jeff!
+print("Hello,")
+print(name)
+
+# Saying hello in a cooler way
+print("Hello," + name)
+
+# Let me show you some rizz
+print("Hello", name)
+
+# Crazy rizz
+print("Hello " end"huzz")
+print(name)
+
+# Saying Hello with a Quotation
+print('hello, "friend"')
+
+# Insist on using double quotes
+print("Hello, \"friend")
+
+
+# Using brackets
+print(f"Hello, {name}")
+
+# Using Strings
+name = name.strip()  # Remove whitespace from str
+
+# Capitalize User's Name
+name = name.capitalize()
+
+# First letter of each word
+name = name.title()
+
+name = name.strip().title()
+
+# Do it all at once
+name = input("What's your name").strip().title()
